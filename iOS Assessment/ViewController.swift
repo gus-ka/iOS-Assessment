b8e58d1644f9abe54fdb253f1467eac6043da84d
Agus Andika <ipg.guska.p@gmail.com>
2022-01-21 20:04:03 +0800

    Initial Commit

---
+++ iOS Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Assessment
+//
+//  Created by Eva Tamara on 21/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
